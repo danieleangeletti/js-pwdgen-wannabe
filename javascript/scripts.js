2a7246790657cff499250de9a75d3e6e1077b04f
danieleangeletti <angelettidaniele@gmail.com>
2023-11-30 14:58:26 +0100

    esercizio completato

---
+++ javascript/scripts.js
@@ -0,0 +1,5 @@
+const nome = prompt("Inserisci il tuo nome:");
+const cognome = prompt("Inserisci il tuo cognome:")
+const colore_preferito = prompt("Inserisci il tuo colore preferito:");
+
+document.getElementById("my-container").innerHTML = nome + cognome + colore_preferito + "114";
